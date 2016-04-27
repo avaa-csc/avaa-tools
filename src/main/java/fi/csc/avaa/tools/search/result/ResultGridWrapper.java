7b067939ff626a7ea4e2069b60053959e580f1da
denissov <nikolai.denissov@gmail.com>
2016-04-27 10:46:43 +0300

    AVAA-647 KuHiTi - Table
    add the custom result control row and CSV printing logics (unfinished)

--- src/main/java/fi/csc/avaa/tools/search/result/ResultGridWrapper.java
+++ src/main/java/fi/csc/avaa/tools/search/result/ResultGridWrapper.java
@@ -18,6 +18,7 @@ public class ResultGridWrapper<T, U> extends VerticalLayout {
 
 	private static final long serialVersionUID = 1L;
 	private ResultControlRow<U> controlRow;
+	private ResultControlSubscriptionCostsRow<U> controlRowSubscriptionCostsRow;
 	private AvaaBaseGrid<T, U> grid;
 	
 	public ResultGridWrapper(AvaaBaseGrid<T, U> grid) {
@@ -33,6 +34,12 @@ public class ResultGridWrapper<T, U> extends VerticalLayout {
 		this(grid);
 		this.controlRow = controlRow;
 		addComponentAsFirst(controlRow);
+	}
+
+	public ResultGridWrapper(AvaaBaseGrid<T, U> grid, ResultControlSubscriptionCostsRow<U> controlRowSubscriptionCostsRow) {
+		this(grid);
+		this.controlRowSubscriptionCostsRow = controlRowSubscriptionCostsRow;
+		addComponentAsFirst(controlRowSubscriptionCostsRow);
 	}
 
 	public void populateGridWithItems(Collection<U> items) {
@@ -57,4 +64,12 @@ public class ResultGridWrapper<T, U> extends VerticalLayout {
 		return controlRow;
 	}
 
+	public ResultControlSubscriptionCostsRow<U> getCurrentControlSubscriptionCostsRow() {
+		return controlRowSubscriptionCostsRow;
+	}
+
+	public void setNewControlRow(ResultControlSubscriptionCostsRow<U> newControlRowSubscriptionCostsRow) {
+		super.replaceComponent(controlRowSubscriptionCostsRow, newControlRowSubscriptionCostsRow);
+		controlRowSubscriptionCostsRow = newControlRowSubscriptionCostsRow;
+	}
 }
